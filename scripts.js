151c9112f1d4619e3bdc46f921e4fa7d675559db
Samuel <sallenellas@gmail.com>
2023-05-04 17:32:35 -0500

    Grid generated with coordinates on 5x2 page grid

--- scripts.js
+++ scripts.js
@@ -1,49 +1,24 @@
+// difficulty
+
+
+// Create a 2D array to hold the buttons
+const buttonGrid = [];
+
+// Set the number of rows and columns for the grid
 const dimensions = 3;
-const playButton = document.querySelector('#play');
 
-
-// Random color generator
-function generateColor(){
-    let color = [];
-    let r = Math.floor(Math.random() * 100) + 50;
-    color.push(r);
-    let g = Math.floor(Math.random() * 100) + 50;
-    color.push(g);
-    let b = Math.floor(Math.random() * 100) + 50;
-    color.push(b);
-    let a = 1.0;
-    color.push(a);
-    color.join('');
-    return color;
-}
-
-playButton.addEventListener('click', () => {
-
-    let row = [];
-    let column = [];
-
-    const squares = document.createElement('div');
-    squares.className = "squares";
-    document.body.appendChild(squares);
-
-    for (let i = 0; i < dimensions; i++){
-        row[i] = document.createElement('button');
-        row[i].classList.add('square');
-        for (let j = 0; j < row.length; j++) {
-            column[j] = document.createElement('button');
-            column[j].classList.add('square');
-            squares.appendChild(column[j]);
-            squares.appendChild(row[i]);
-        }
-        squares.appendChild(row[i]);
+// Create the buttons and add them to the button grid
+for (let row = 0; row < dimensions; row++) {
+buttonGrid[row] = [];
+    for (let col = 0; col < dimensions; col++) {
+        const button = document.createElement("button");
+        button.textContent = `(${row}, ${col})`;
+        buttonGrid[row][col] = button;
+        button.setAttribute("id", `(${row}, ${col})`);
+        button.classList.add('square');
+        // button.classList.add('fade-in');
+        // button.style.gridRow = row + 1;
+        // button.style.gridColumn = col + 1;
+        document.getElementById("button-grid").appendChild(button);
     }
-    squares.addEventListener('click', (e) => {
-        // console.log(e.target);
-        let red = generateColor()[0];
-        let green = generateColor()[1];
-        let blue = generateColor()[2];
-        let alpha = generateColor()[3];
-        e.target.style = `background-color: #${red}${green}${blue}${alpha}`;
-    });
-
-});
+};
