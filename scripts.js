477e8ae3e438afb876ab89851419ff32e2f8ac11
Samuel <sallenellas@gmail.com>
2023-05-03 22:17:11 -0500

    Basic color changing functionality for buttons on click

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
 const dimensions = 3;
 const playButton = document.querySelector('#play');
 
+
+// Random color generator
+function generateColor(){
+    let color = [];
+    let r = Math.floor(Math.random() * 100) + 50;
+    color.push(r);
+    let g = Math.floor(Math.random() * 100) + 50;
+    color.push(g);
+    let b = Math.floor(Math.random() * 100) + 50;
+    color.push(b);
+    let a = 1.0;
+    color.push(a);
+    color.join('');
+    return color;
+}
+
 playButton.addEventListener('click', () => {
 
     let row = [];
@@ -12,19 +28,22 @@ playButton.addEventListener('click', () => {
 
     for (let i = 0; i < dimensions; i++){
         row[i] = document.createElement('button');
-        row[i].className = "row";
         row[i].classList.add('square');
-        squares.appendChild(row[i]);
-
         for (let j = 0; j < row.length; j++) {
             column[j] = document.createElement('button');
-            column[j].className = "column";
             column[j].classList.add('square');
             squares.appendChild(column[j]);
+            squares.appendChild(row[i]);
         }
+        squares.appendChild(row[i]);
     }
+    squares.addEventListener('click', (e) => {
+        // console.log(e.target);
+        let red = generateColor()[0];
+        let green = generateColor()[1];
+        let blue = generateColor()[2];
+        let alpha = generateColor()[3];
+        e.target.style = `background-color: #${red}${green}${blue}${alpha}`;
+    });
 
 });
-
-
-
